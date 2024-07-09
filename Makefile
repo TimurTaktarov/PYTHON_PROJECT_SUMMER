90ac8a93b705a96ca8047a5bb3d86a27b21275ca
timur <timurka.t.r@gmail.com>
2024-07-09 19:29:18 +0300

    templating

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ run:
 	uvicorn app.main:app --reload --port 8000
 
 
-.PHONY: docs
-docs:
+.PHONY: webdocs
+webdocs:
 	mkdocs serve -a 127.0.0.1:5000
--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
 from app.settings import settings
 
 
@@ -6,11 +8,22 @@ app = FastAPI(
     title=settings.APP_NAME,
     description="we are the champions",
 )
+templates = Jinja2Templates(directory=Path(__file__).parent.parent / 'templates')
 
 
 @app.get('/mkdocs')
 async def documentation():
     pass
+
+
+@app.get('/')
+def greetings(request: Request, message: str = 'HELLO WORLD!'):
+    context = {
+        "title": message,
+        "request": request,
+    }
+    response = templates.TemplateResponse('base.html', context=context)
+    return response
 
 
 if __name__ == "__main__":  # pragma: no cover
